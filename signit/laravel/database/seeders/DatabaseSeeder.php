95b7ae7a6022d0b1a28917893f7e2ecc221c9f62
Tom <tom_altham@hotmail.co.uk>
2021-11-04 16:06:10 +0000

    Add a visitor model with migrations and seeder. Add to controller and view

--- signit/laravel/database/seeders/DatabaseSeeder.php
+++ signit/laravel/database/seeders/DatabaseSeeder.php
@@ -3,16 +3,14 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\VisitorSeeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            VisitorSeeder::class
+        ]);
     }
 }
---
+++ signit/laravel/database/seeders/VisitorSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Visitor;
+
+class VisitorSeeder extends Seeder
+{
+    public function run()
+    {
+        Visitor::factory()
+            ->count(50)
+            ->create();
+    }
+}
