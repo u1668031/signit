9289a357fd63bae04edea10c4b07374b270007c3
Tom <tom_altham@hotmail.co.uk>
2021-11-06 14:13:20 +0000

    Add Login view/controller/routes

--- signit/laravel/routes/web.php
+++ signit/laravel/routes/web.php
@@ -3,9 +3,14 @@
 <?php
 use App\Http\Controllers\VisitorController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\WelcomeController;
 Route::get('/', [WelcomeController::class, 'index']);
 Route::resource('/visitors', VisitorController::class);
+Route::get('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/login', [LoginController::class, 'authenticate'])->name('authenticate');
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
